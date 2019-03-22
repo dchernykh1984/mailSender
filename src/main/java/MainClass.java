fae98e883c54b6479d9170c566439da0bb38b151
Denis Chernykh <denis_chernykh@epam.com>
2019-03-22 14:12:11 +0300

    init

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,21 @@
+import javax.mail.Session;
+import java.util.Properties;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.out.println("SimpleEmail Start");
+
+        String smtpHostServer = "localhost";
+        String emailID = "dchernykh@localhost.mock";
+
+        Properties props = System.getProperties();
+
+        props.put("mail.smtp.host", smtpHostServer);
+
+        Session session = Session.getInstance(props, null);
+
+        EmailUtil.sendEmail(session, emailID, "Email for experiments with SMTP mock", "SimpleEmail Testing Body. To check how we can operate and control SMTP mock");
+
+    }
+}
